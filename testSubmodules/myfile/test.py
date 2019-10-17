0383778c21f8d9c80c9a40600424d1085182af3f
liyp0095 <liyp0095@gmail.com>
2019-10-16 23:50:41 -0500

    test submodule

---
+++ testSubmodules/myfile/test.py
@@ -0,0 +1,18 @@
+#!/bin/python
+#-*- coding: utf8 -*-
+
+import sys
+sys.path.append("../")
+sys.path.append("../aima_python")
+
+# import importlib
+# Game = importlib.import_module("aima-python.games")
+from aima_python.games import Game
+
+
+def main():
+    g = Game()
+
+
+if __name__ == '__main__':
+    main()
